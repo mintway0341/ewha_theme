1fa76f9aeaae102206903e40a07f224629d4cc5b
Yoomin Kang <44641348+mintway0341@users.noreply.github.com>
2022-01-15 00:43:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function download_aos()
+{
+    // window.open("https://play.google.com/store/apps/details?id=com.mintway_dev.theme.korea_university");
+}
+function download_ios()
+{
+    // window.location.assign('Korea University.ktheme');
+}
+function visit_instagram()
+{
+    window.open("https://www.instagram.com/u_mini.341/");
+}
+function visit_github()
+{
+    window.open("https://github.com/mintway0341");
+}
+
+function change_color_1_aos()
+{
+    document.getElementById("button_download_aos").style.backgroundColor = '#002c1a';
+    document.getElementById("button_download_aos").style.boxShadow = "0px 0px 10px #00462a40";
+}
+function change_color_2_aos()
+{
+    document.getElementById("button_download_aos").style.backgroundColor = '#00462a';
+    document.getElementById("button_download_aos").style.boxShadow = "0px 0px 0px #00462a40";
+}
+function change_color_1_ios()
+{
+    document.getElementById("button_download_ios").style.backgroundColor = '#002c1a';
+    document.getElementById("button_download_ios").style.boxShadow = "0px 0px 10px #00462a40";
+}
+function change_color_2_ios()
+{
+    document.getElementById("button_download_ios").style.backgroundColor = '#00462a';
+    document.getElementById("button_download_ios").style.boxShadow = "0px 0px 0px #00462a40";
+}
+function change_color_1_instagram()
+{
+    document.getElementById("button_instagram").style.backgroundColor = '#002c1a';
+    document.getElementById("button_instagram").style.boxShadow = "0px 0px 10px #00462a40";
+}
+function change_color_2_instagram()
+{
+    document.getElementById("button_instagram").style.backgroundColor = '#00462a';
+    document.getElementById("button_instagram").style.boxShadow = "0px 0px 0px #00462a40";
+}
+function change_color_1_github()
+{
+    document.getElementById("button_github").style.backgroundColor = '#002c1a';
+    document.getElementById("button_github").style.boxShadow = "0px 0px 10px #00462a40";
+}
+function change_color_2_github()
+{
+    document.getElementById("button_github").style.backgroundColor = '#00462a';
+    document.getElementById("button_github").style.boxShadow = "0px 0px 0px #00462a40";
+}
